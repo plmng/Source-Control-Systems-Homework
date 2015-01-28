e1b3af326d8224acc1cc9a21c53fe27b99ba1800
Plamena Georgieva <plmng13@gmail.com>
2015-01-28 16:05:10 +0200

    c# Trailing Zeroes
    part of C# homework

---
+++ TrailingZeroesinN/TrailingZeroesInN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Write a program that calculates with how many zeroes the factorial of a given number n has at its end. Your program should work well for very big numbers, e.g. n=100000
+class TrailingZeroesInN
+{
+    static void Main()
+    {
+        Console.Write("Enter integer number: ");
+        int n;
+        if (int.TryParse(Console.ReadLine().ToString(), out n))
+        {
+            int countZeroes=0;
+            for (int i = 5; i <= n; i*=5)
+            {
+                countZeroes+= n/i;
+            }
+            Console.WriteLine(countZeroes);
+        }
+        else Console.WriteLine("entered data is not correct");
+    }
+}
+
