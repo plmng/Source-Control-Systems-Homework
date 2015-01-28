d9bd98bb53c500e7dbc5c2a161a70391d868bb0a
Plamena Georgieva <plmng13@gmail.com>
2015-01-28 17:31:10 +0200

    Part of C# homework
    Signed-off-by: Plamena Georgieva <plmng13@gmail.com>

---
+++ BinaryToDecimalNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class BinaryToDecimalNumber
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("enter binary number: ");
+            string input = Console.ReadLine();
+            long decRepresent = 0;
+            int[] number = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                number[i] = int.Parse(input[i].ToString());
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != 0 && number[i] !=1)
+                {
+                    Console.WriteLine("entered data is not correct");
+                    return;
+                }
+            }
+            for (int i = 0, j=number.Length-1; i < number.Length; i++, j--)
+            {
+                decRepresent += number[i] * (long)Math.Pow(2,j);
+            }
+            Console.WriteLine(decRepresent);
+        }
+        catch (FormatException)
+        {
+           Console.WriteLine("entered data is not correct");
+        }
+
+    }
+}
---
+++ CalculateGCD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class CalculateGCD
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("enter value for number a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("enter value for number b: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a;
+            if (a > b)
+            {
+                a = b;
+                b = c;
+            }
+            while (b != 0)
+            {
+                c = b;
+                b = a % b;
+                a = c;
+            }
+            Console.WriteLine(a);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entered data is not correct");
+        }
+    }
+}
